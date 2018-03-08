3eb4e181052cacdee8d46173e0e67575c8cca07c
Michael Spear <mwspear@gmail.com>
2018-03-08 16:59:08 -0600

    Created working code editor with executable code

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask
-from wtforms import form
+from flask import Flask, render_template, request
+from wtforms import Form
 from wtforms.fields import SubmitField
-from flask.ext.codemirror import CodeMirror
-from flask.ext.codemirror.fields import CodeMirrorField
+from flask_codemirror import CodeMirror
+from flask_codemirror.fields import CodeMirrorField
 from flask_assets import Bundle, Environment
+import subprocess
 
-class MyFrom(form):
-    source_code = CodeMirrorField(language='python3', config={'lineNumbers': True})
+
+class MyForm(Form):
+    source_code = CodeMirrorField(language='python', config={'lineNumbers': True})
     submit = SubmitField('Submit')
 
-CODEMIRROR_LANGUAGES = ['python3', 'html']
+
+CODEMIRROR_LANGUAGES = ['python', 'html']
+
 app = Flask(__name__)
 
 app.config.from_object(__name__)
-bundles = { # Bundle our javascript files
+codemirror = CodeMirror(app)
+bundles = {  # Bundle our javascript files
     'home_js': Bundle(
         'webgazer.js',
         'jquery-3.3.1.js',
@@ -25,9 +30,28 @@ bundles = { # Bundle our javascript files
 assets = Environment(app)
 assets.register(bundles)
 
-@app.route('/')
-def hello_world():
-    return "Hello World"
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = MyForm(request.form)
+    if request.method == 'POST' and form.validate():
+        text = form.source_code.data
+    else:
+        text = ''
+
+    def inner():
+        proc = subprocess.Popen(
+            ['python', '-c', text],
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        for line in iter(proc.stdout.readline, b''):
+            yield line.decode().rstrip() + '\n'
+
+    return render_template('index.html', form=form, postback=inner() if text else '')
 
 
 if __name__ == '__main__':
