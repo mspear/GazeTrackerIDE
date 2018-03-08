79f263c06f3407e264b6d39de28a1dc9ebff5d27
Michael Spear <mwspear@gmail.com>
2018-03-08 15:00:54 -0600

    began adding codemirror stuff

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from wtforms import form
+from wtforms.fields import SubmitField
+from flask.ext.codemirror import CodeMirror
+from flask.ext.codemirror.fields import CodeMirrorField
 from flask_assets import Bundle, Environment
 
+class MyFrom(form):
+    source_code = CodeMirrorField(language='python3', config={'lineNumbers': True})
+    submit = SubmitField('Submit')
+
+CODEMIRROR_LANGUAGES = ['python3', 'html']
 app = Flask(__name__)
 
+app.config.from_object(__name__)
 bundles = { # Bundle our javascript files
     'home_js': Bundle(
         'webgazer.js',
