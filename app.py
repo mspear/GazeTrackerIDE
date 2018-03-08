ddca582d7cc6f748ba52f219f74587c5be0a8df9
Michael Spear <mwspear@gmail.com>
2018-03-07 21:13:56 -0600

    Added and bundled jsfiles

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_assets import Bundle, Environment
 
 app = Flask(__name__)
 
+bundles = { # Bundle our javascript files
+    'home_js': Bundle(
+        'webgazer.js',
+        'jquery-3.3.1.js',
+        output='gen/home.js',
+        filters='jsmin',
+    )
+}
+
+assets = Environment(app)
+assets.register(bundles)
 
 @app.route('/')
 def hello_world():
